7e8c42dba07817db82990177f08de829c9d78708
Alex Emelyanov <holyketzer@gmail.com>
2020-04-18 21:29:10 +0300

    Add test case for loading of previously selected values in multi-value field

--- spec/dummy/app/models/item.rb
+++ spec/dummy/app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ActiveRecord::Base
   validates :name, presence: true
 
   belongs_to :subcategory
+  has_and_belongs_to_many :tags
 end
--- spec/support/selectize_helpers.rb
+++ spec/support/selectize_helpers.rb
@@ -6,9 +6,10 @@ module SelectizeHelpers
   end
 
   # Get current selectize items
-  def selectize_items(selector = selectize_selector)
+  def selectize_items_in_dropdown(selector = selectize_selector)
     # Wait until selectize renders with blocking find
     select_tag = find("#{selector} select", visible: false)
+    select_option = first("#{selector} .selectize-dropdown-content .item", visible: false)
 
     all("#{selector} .selectize-dropdown-content .item", visible: false).map do |div|
       {
@@ -17,5 +18,33 @@ module SelectizeHelpers
         selected: div[:class].include?('selected')
       }
     end
+  end
+
+  def selectize_items_in_field(selector = selectize_selector)
+    # Wait until selectize renders with blocking find
+    select_tag = find("#{selector} select", visible: false)
+    select_option = first("#{selector} .selectize-input.items div", visible: false)
+
+    all("#{selector} .selectize-input.items div", visible: false).map do |div|
+      {
+        value: div[:'data-value'],
+        text: div.text,
+        selected: true,
+      }
+    end
+  end
+
+  def wait_item(timeout = 15, &block)
+    started_at = Time.current
+
+    while !(res = block.call) do
+      sleep(1)
+
+      if Time.current - started_at > timeout
+        raise RuntimeError, "timeout #{timeout} sec."
+      end
+    end
+
+    res
   end
 end
