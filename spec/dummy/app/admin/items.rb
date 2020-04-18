be13677c9ae60f7657cb0fa6eeda954d4a2ad4a2
Alex Emelyanov <holyketzer@gmail.com>
2020-04-18 21:29:10 +0300

    Add test case for loading of previously selected values in multi-value field

--- spec/dummy/app/admin/items.rb
+++ spec/dummy/app/admin/items.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Item do
-  permit_params :id, :name, :subcategory_id
+  permit_params :id, :name, :subcategory_id, tag_ids: []
   config.sort_order = 'id_asc'
 
   form do |f|
@@ -7,12 +7,19 @@ ActiveAdmin.register Item do
 
     f.inputs do
       f.input :name
+
       f.input :subcategory, as: :ajax_select, data: {
         search_fields: [:name],
         url: '/admin/subcategories/filter',
         limit: Subcategory::AJAX_LIMIT
       }
+
+      f.input :tags, as: :ajax_select, data: {
+        search_fields: ['name'],
+        url: '/admin/tags/filter'
+      }
     end
+
 
     f.actions
   end
